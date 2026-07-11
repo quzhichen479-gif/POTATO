1c32e082b9bdd5e8a1efa591bf301f97c6f286fd
quzhichen479-gif <quzhichen479@gmail.com>
2026-07-11 21:33:47 +0800

    Add potato_e1 package initializer

---
+++ src/potato_e1/__init__.py
@@ -0,0 +1,6 @@
+"""POTATO E1 lightweight Transformer candidate arbitrator."""
+
+from .model import CandidateTransformerArbitrator
+
+__all__ = ["CandidateTransformerArbitrator"]
+__version__ = "0.1.0"
