f2d7325158f9a19a5e626d217ddd1c4537e49c4b
quzhichen479-gif <quzhichen479@gmail.com>
2026-07-12 13:13:13 +0800

    Expose E1.2 rollback Transformer

--- src/potato_e1/__init__.py
+++ src/potato_e1/__init__.py
@@ -1,7 +1,12 @@
-"""POTATO candidate arbitration research package."""
+"""POTATO RGB-polarization candidate arbitration research package."""
 
 from .e11_model import CandidateResidualTransformer
+from .e12_model import RollbackTransformer
 from .model import CandidateTransformerArbitrator
 
-__all__ = ["CandidateTransformerArbitrator", "CandidateResidualTransformer"]
-__version__ = "0.2.0"
+__all__ = [
+    "CandidateTransformerArbitrator",
+    "CandidateResidualTransformer",
+    "RollbackTransformer",
+]
+__version__ = "0.3.0"
