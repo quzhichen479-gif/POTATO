264e7656dd104add36d577910f4e42baf7c0bc54
quzhichen479-gif <quzhichen479@gmail.com>
2026-07-12 10:59:28 +0800

    Expose E1.1 residual Transformer

--- src/potato_e1/__init__.py
+++ src/potato_e1/__init__.py
@@ -1,6 +1,7 @@
-"""POTATO E1 lightweight Transformer candidate arbitrator."""
+"""POTATO candidate arbitration research package."""
 
+from .e11_model import CandidateResidualTransformer
 from .model import CandidateTransformerArbitrator
 
-__all__ = ["CandidateTransformerArbitrator"]
-__version__ = "0.1.0"
+__all__ = ["CandidateTransformerArbitrator", "CandidateResidualTransformer"]
+__version__ = "0.2.0"
